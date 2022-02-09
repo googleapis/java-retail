0e259836270f3d4598b4fc569de0ce3a3b285c7b
Sergey Borisenko <sborisenko@griddynamics.com>
2022-02-09 13:52:49 +0200

    Add search code samples.

---
+++ samples/interactive-tutorials/src/main/java/search/SearchWithFiltering.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * [START retail_search_for_products_with_filter]
+ * Call Retail API to search for a products in a catalog,
+ * filter the results by different product fields.
+ */
+
+package search;
+
+import com.google.cloud.retail.v2.SearchRequest;
+import com.google.cloud.retail.v2.SearchResponse;
+import com.google.cloud.retail.v2.SearchServiceClient;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class SearchWithFiltering {
+
+  public static void main(final String[] args) throws IOException {
+    // TODO(developer): Replace these variables before running the sample.
+    String projectNumber = System.getenv("PROJECT_NUMBER");
+    String defaultCatalogName = String.format(
+        "projects/%s/locations/global/catalogs/default_catalog", projectNumber);
+    String defaultSearchPlacementName =
+        defaultCatalogName + "/placements/default_search";
+    String defaultBranchName =
+        defaultCatalogName + "/branches/default_branch";
+
+    search(defaultSearchPlacementName, defaultBranchName);
+  }
+
+  public static SearchResponse search(String defaultSearchPlacementName,
+      String defaultBranchName) throws IOException {
+    // TRY DIFFERENT FILTER EXPRESSIONS HERE:
+    String filter = "(colorFamilies: ANY(\"Black\"))";
+    String queryPhrase = "Tee";
+    final int pageSize = 10;
+    String visitorId = UUID.randomUUID().toString();
+
+    SearchRequest searchRequest = SearchRequest.newBuilder()
+        .setPlacement(defaultSearchPlacementName)
+        .setBranch(defaultBranchName)
+        .setVisitorId(visitorId)
+        .setQuery(queryPhrase)
+        .setPageSize(pageSize)
+        .setFilter(filter)
+        .build();
+
+    System.out.println("Search request: " + searchRequest);
+
+    SearchResponse searchResponse = SearchServiceClient.create().search(
+        searchRequest).getPage().getResponse();
+    System.out.println("Search response: " + searchResponse);
+
+    return searchResponse;
+  }
+}
+
+// [END retail_search_for_products_with_filter]
