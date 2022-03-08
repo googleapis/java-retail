52ad4bbc04a051a0c61ee1e616709fa58317e329
Piotr Michalski <pmichalski@griddynamics.com>
2022-03-08 10:11:10 +0100

    * removed base class and made examples self-contained

--- samples/interactive-tutorials/src/main/java/product/ImportProductsBigQueryTable.java
+++ samples/interactive-tutorials/src/main/java/product/ImportProductsBigQueryTable.java
@@ -23,12 +23,17 @@
 package product;
 
 import com.google.cloud.retail.v2.BigQuerySource;
+import com.google.cloud.retail.v2.ImportMetadata;
 import com.google.cloud.retail.v2.ImportProductsRequest;
 import com.google.cloud.retail.v2.ImportProductsRequest.ReconciliationMode;
+import com.google.cloud.retail.v2.ImportProductsResponse;
 import com.google.cloud.retail.v2.ProductInputConfig;
+import com.google.cloud.retail.v2.ProductServiceClient;
+import com.google.longrunning.Operation;
+import com.google.longrunning.OperationsClient;
 import java.io.IOException;
 
-public class ImportProductsBigQueryTable extends ImportProductsBase {
+public class ImportProductsBigQueryTable {
 
   private static final String PROJECT_ID = System.getenv("PROJECT_ID");
   private static final String DEFAULT_CATALOG =
@@ -71,6 +76,38 @@ public class ImportProductsBigQueryTable extends ImportProductsBase {
     System.out.printf("Import products from big query table request: %s%n", importRequest);
 
     return importRequest;
+  }
+
+  private static void waitForOperationCompletion(ImportProductsRequest importRequest)
+      throws IOException, InterruptedException {
+    try (ProductServiceClient serviceClient = ProductServiceClient.create()) {
+      String operationName = serviceClient.importProductsCallable().call(importRequest).getName();
+      System.out.printf("OperationName = %s\n", operationName);
+
+      OperationsClient operationsClient = serviceClient.getOperationsClient();
+      Operation operation = operationsClient.getOperation(operationName);
+
+      while (!operation.getDone()) {
+        // Keep polling the operation periodically until the import task is done.
+        int awaitDuration = 30000;
+        Thread.sleep(awaitDuration);
+        operation = operationsClient.getOperation(operationName);
+      }
+
+      if (operation.hasMetadata()) {
+        ImportMetadata metadata = operation.getMetadata().unpack(ImportMetadata.class);
+        System.out.printf(
+            "Number of successfully imported products: %s\n", metadata.getSuccessCount());
+        System.out.printf(
+            "Number of failures during the importing: %s\n", metadata.getFailureCount());
+      }
+
+      if (operation.hasResponse()) {
+        ImportProductsResponse response =
+            operation.getResponse().unpack(ImportProductsResponse.class);
+        System.out.printf("Operation result: %s%n", response);
+      }
+    }
   }
 }
 
--- samples/interactive-tutorials/src/main/java/product/ImportProductsInlineSource.java
+++ samples/interactive-tutorials/src/main/java/product/ImportProductsInlineSource.java
@@ -24,11 +24,16 @@ package product;
 
 import com.google.cloud.retail.v2.ColorInfo;
 import com.google.cloud.retail.v2.FulfillmentInfo;
+import com.google.cloud.retail.v2.ImportMetadata;
 import com.google.cloud.retail.v2.ImportProductsRequest;
+import com.google.cloud.retail.v2.ImportProductsResponse;
 import com.google.cloud.retail.v2.PriceInfo;
 import com.google.cloud.retail.v2.Product;
 import com.google.cloud.retail.v2.ProductInlineSource;
 import com.google.cloud.retail.v2.ProductInputConfig;
+import com.google.cloud.retail.v2.ProductServiceClient;
+import com.google.longrunning.Operation;
+import com.google.longrunning.OperationsClient;
 import com.google.protobuf.FieldMask;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -37,7 +42,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-public class ImportProductsInlineSource extends ImportProductsBase {
+public class ImportProductsInlineSource {
 
   private static final String PROJECT_ID = System.getenv("PROJECT_ID");
   private static final String DEFAULT_CATALOG =
@@ -168,6 +173,38 @@ public class ImportProductsInlineSource extends ImportProductsBase {
     products.add(product2);
 
     return products;
+  }
+
+  private static void waitForOperationCompletion(ImportProductsRequest importRequest)
+      throws IOException, InterruptedException {
+    try (ProductServiceClient serviceClient = ProductServiceClient.create()) {
+      String operationName = serviceClient.importProductsCallable().call(importRequest).getName();
+      System.out.printf("OperationName = %s\n", operationName);
+
+      OperationsClient operationsClient = serviceClient.getOperationsClient();
+      Operation operation = operationsClient.getOperation(operationName);
+
+      while (!operation.getDone()) {
+        // Keep polling the operation periodically until the import task is done.
+        int awaitDuration = 30000;
+        Thread.sleep(awaitDuration);
+        operation = operationsClient.getOperation(operationName);
+      }
+
+      if (operation.hasMetadata()) {
+        ImportMetadata metadata = operation.getMetadata().unpack(ImportMetadata.class);
+        System.out.printf(
+            "Number of successfully imported products: %s\n", metadata.getSuccessCount());
+        System.out.printf(
+            "Number of failures during the importing: %s\n", metadata.getFailureCount());
+      }
+
+      if (operation.hasResponse()) {
+        ImportProductsResponse response =
+            operation.getResponse().unpack(ImportProductsResponse.class);
+        System.out.printf("Operation result: %s%n", response);
+      }
+    }
   }
 }
 
