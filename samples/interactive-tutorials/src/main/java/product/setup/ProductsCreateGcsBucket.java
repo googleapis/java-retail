855fa6990c1cd323828ad4c7b71191b15a7c8855
Sergey Borisenko <sborisenko@griddynamics.com>
2022-04-25 15:55:56 +0300

    Refactoring product package (Import).

--- samples/interactive-tutorials/src/main/java/product/setup/ProductsCreateGcsBucket.java
+++ samples/interactive-tutorials/src/main/java/product/setup/ProductsCreateGcsBucket.java
@@ -26,31 +26,31 @@ import java.time.Instant;
 
 public class ProductsCreateGcsBucket {
 
-  private static final String PROJECT_ID = ServiceOptions.getDefaultProjectId();
+  public static void main(String[] args) throws IOException {
+    String projectId = ServiceOptions.getDefaultProjectId();
 
-  private static final Timestamp CURRENT_DATE =
-      Timestamp.newBuilder()
-          .setSeconds(Instant.now().getEpochSecond())
-          .setNanos(Instant.now().getNano())
-          .build();
+    Timestamp currentDate =
+        Timestamp.newBuilder()
+            .setSeconds(Instant.now().getEpochSecond())
+            .setNanos(Instant.now().getNano())
+            .build();
 
-  private static final String BUCKET_NAME =
-      String.format("%s_products_%s", PROJECT_ID, CURRENT_DATE.getSeconds());
+    String productsBucketName =
+        String.format("%s_products_%s", projectId, currentDate.getSeconds());
 
-  public static void main(String... args) throws IOException {
-    createBucket(BUCKET_NAME);
-    System.out.printf("Products gcs bucket %s was created.", BUCKET_NAME);
-
-    uploadObject(BUCKET_NAME, "products.json", "src/main/resources/products.json");
-    System.out.printf("File 'products.json' was uploaded into bucket '%s'.", BUCKET_NAME);
-
-    uploadObject(
-        BUCKET_NAME, "products_some_invalid.json", "src/main/resources/products_some_invalid.json");
-    System.out.printf(
-        "File 'products_some_invalid.json' was uploaded into bucket '%s'.", BUCKET_NAME);
+    createGcsBucketAndUploadData(productsBucketName);
   }
 
-  public static String getBucketName() {
-    return BUCKET_NAME;
+  public static void createGcsBucketAndUploadData(String bucketName) throws IOException {
+    createBucket(bucketName);
+    System.out.printf("Products gcs bucket %s was created.%n", bucketName);
+
+    uploadObject(bucketName, "products.json", "src/main/resources/products.json");
+    System.out.printf("File 'products.json' was uploaded into bucket '%s'.%n", bucketName);
+
+    uploadObject(
+        bucketName, "products_some_invalid.json", "src/main/resources/products_some_invalid.json");
+    System.out.printf(
+        "File 'products_some_invalid.json' was uploaded into bucket '%s'.%n", bucketName);
   }
 }
--- samples/interactive-tutorials/src/test/java/product/setup/RemoveProductsResourcesTest.java
+++ samples/interactive-tutorials/src/test/java/product/setup/RemoveProductsResourcesTest.java
@@ -1,23 +1,9 @@
-/*
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package product;
+package product.setup;
 
 import static com.google.common.truth.Truth.assertThat;
-import static product.ImportProductsInlineSource.importProductsInlineSource;
+import static product.setup.RemoveProductsResources.deleteAllProducts;
+import static setup.SetupCleanup.deleteBucket;
+import static setup.SetupCleanup.deleteDataset;
 
 import com.google.cloud.ServiceOptions;
 import java.io.ByteArrayOutputStream;
@@ -31,7 +17,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-public class ImportProductsInlineSourceTest {
+public class RemoveProductsResourcesTest {
 
   private ByteArrayOutputStream bout;
   private PrintStream originalPrintStream;
@@ -39,24 +25,27 @@ public class ImportProductsInlineSourceTest {
   @Before
   public void setUp() throws IOException, InterruptedException, ExecutionException {
     String projectId = ServiceOptions.getDefaultProjectId();
+    String bucketName = "products_tests_bucket";
     String branchName =
         String.format(
             "projects/%s/locations/global/catalogs/default_catalog/branches/0", projectId);
+
     bout = new ByteArrayOutputStream();
     PrintStream out = new PrintStream(bout);
     originalPrintStream = System.out;
     System.setOut(out);
 
-    importProductsInlineSource(branchName);
+    deleteBucket(bucketName);
+    deleteAllProducts(branchName);
+    deleteDataset(projectId, "products");
   }
 
   @Test
-  public void testImportProductsInlineSource() {
+  public void testRemoveProductsResources() {
     String outputResult = bout.toString();
 
-    assertThat(outputResult).contains("Import products from inline source request");
-    assertThat(outputResult).contains("Number of successfully imported products");
-    assertThat(outputResult).contains("Number of failures during the importing");
+    assertThat(outputResult).contains("Deleting products in process, please wait...");
+    assertThat(outputResult).contains("products were deleted from");
   }
 
   @After
